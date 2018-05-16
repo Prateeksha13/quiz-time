f7580c0b7e6eb1c70a9660ef41d129c308e3cce5
Prateeksha13 <prateekshanishi@gmail.com>
2018-05-16 22:15:16 +0530

    Add component which displays the result of the quiz using chart.js bar graph

---
+++ src/components/resultRenderer.jsx
@@ -0,0 +1,24 @@
+import {Bar} from 'react-chartjs-2';
+import React from 'react';
+
+class ResultRenderer extends React.Component {
+    render() {
+        return (
+            <Bar
+                data={this.props.chartData}
+                options={{
+                    maintainAspectRatio: false,
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }}
+            />
+        );
+    }
+}
+
+export default ResultRenderer;
